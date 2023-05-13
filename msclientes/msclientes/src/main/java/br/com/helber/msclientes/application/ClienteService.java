779d6312f5d8963c88ae1f255d327684cf860450
HelberLuz <helbersap@gmail.com>
2023-05-12 22:00:33 -0300

    implementação do ClienteService.java e ClienteSaveRequest.java

--- msclientes/msclientes/src/main/java/br/com/helber/msclientes/application/ClienteService.java
+++ msclientes/msclientes/src/main/java/br/com/helber/msclientes/application/ClienteService.java
@@ -20,7 +20,7 @@ public class ClienteService {
     }
 
     public Optional<Cliente> getByCPF(String cpf){
-        return repository.finByCpf(cpf);
+        return repository.findByCpf(cpf);
 
     }
 }
