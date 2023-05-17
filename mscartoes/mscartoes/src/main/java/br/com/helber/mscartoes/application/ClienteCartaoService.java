df8b16e827bb75a75bf739821af0ac52caba2279
HelberLuz <helbersap@gmail.com>
2023-05-17 14:52:04 -0300

    implementação CartoesPorClienteResponse.java e ClienteCartaoRepository.java

---
+++ mscartoes/mscartoes/src/main/java/br/com/helber/mscartoes/application/ClienteCartaoService.java
@@ -0,0 +1,18 @@
+package br.com.helber.mscartoes.application;
+
+import br.com.helber.mscartoes.domain.ClienteCartao;
+import br.com.helber.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteCartaoService {
+    private final ClienteCartaoRepository repository;
+
+    public List<ClienteCartao> listCartoesByCpf(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
---
+++ mscartoes/mscartoes/src/main/java/br/com/helber/mscartoes/infra/repository/ClienteCartaoRepository.java
@@ -0,0 +1,11 @@
+package br.com.helber.mscartoes.infra.repository;
+
+import br.com.helber.mscartoes.domain.ClienteCartao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClienteCartaoRepository extends JpaRepository<ClienteCartao, Long> {
+    List<ClienteCartao> findByCpf(String cpf);
+
+}
