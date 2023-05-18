1f479c2b67ae6d567138f7d8f8220bf62edf34ca
HelberLuz <helbersap@gmail.com>
2023-05-18 10:59:23 -0300

    implementação do reteamento do avaliador de credito no gateway

---
+++ msavaliadorcredito/msavaliadorcredito/src/test/java/bc/com/helber/msavaliadorcredito/MsavaliadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package bc.com.helber.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
