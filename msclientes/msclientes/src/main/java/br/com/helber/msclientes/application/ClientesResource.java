3e87d864a2f2a452bd5b116237c5671703547b2c
HelberLuz <helbersap@gmail.com>
2023-05-22 11:37:29 -0300

    implementação do keycloak e logs

--- msclientes/msclientes/src/main/java/br/com/helber/msclientes/application/ClientesResource.java
+++ msclientes/msclientes/src/main/java/br/com/helber/msclientes/application/ClientesResource.java
@@ -4,6 +4,8 @@ import br.com.helber.msclientes.application.representation.ClienteSaveRequest;
 import br.com.helber.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
